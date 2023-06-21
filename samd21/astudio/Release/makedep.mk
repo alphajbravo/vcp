4a60c91712a033206c5c18793c6da9ba7afe04f8
Andrew Berry <andrew@x-laser.com>
2023-06-21 12:36:12 -0400

    add samd21 project

---
+++ samd21/astudio/Release/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+..\startup_samd11.c
+
+..\main.c
+
+..\uart.c
+
+..\usb\usb_cdc.c
+
+..\usb\usb_samd11.c
+
+..\usb_descriptors.c
+
+..\usb\usb_std.c
+
---
+++ samd21/usb_descriptors.c
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2017-2022, Alex Taradov <alex@taradov.com>. All rights reserved.
+
+/*- Includes ----------------------------------------------------------------*/
+#include <stdalign.h>
+#include "usb_descriptors.h"
+
+/*- Variables ---------------------------------------------------------------*/
+const alignas(4) usb_device_descriptor_t usb_device_descriptor =
+{
+  .bLength            = sizeof(usb_device_descriptor_t),
+  .bDescriptorType    = USB_DEVICE_DESCRIPTOR,
+  .bcdUSB             = USB_BCD_VERSION,
+  .bDeviceClass       = USB_DEVICE_CLASS_MISCELLANEOUS,
+  .bDeviceSubClass    = USB_DEVICE_SUBCLASS_COMMON,
+  .bDeviceProtocol    = USB_DEVICE_PROTOCOL_INTERFACE_ASSOCIATION,
+  .bMaxPacketSize0    = USB_CTRL_EP_SIZE,
+  .idVendor           = 0x6666,
+  .idProduct          = 0x8800,
+  .bcdDevice          = 0x0100,
+  .iManufacturer      = USB_STR_MANUFACTURER,
+  .iProduct           = USB_STR_PRODUCT,
+  .iSerialNumber      = USB_STR_SERIAL_NUMBER,
+  .bNumConfigurations = 1,
+};
+
+const alignas(4) usb_configuration_hierarchy_t usb_configuration_hierarchy =
+{
+  .configuration =
+  {
+    .bLength             = sizeof(usb_configuration_descriptor_t),
+    .bDescriptorType     = USB_CONFIGURATION_DESCRIPTOR,
+    .wTotalLength        = sizeof(usb_configuration_hierarchy_t),
+    .bNumInterfaces      = USB_INTF_COUNT,
+    .bConfigurationValue = 1,
+    .iConfiguration      = 0,
+    .bmAttributes        = USB_ATTRIBUTE_BUS_POWERED,
+    .bMaxPower           = USB_MAX_POWER(500),
+  },
+
+  .iad =
+  {
+    .bLength             = sizeof(usb_interface_association_descriptor_t),
+    .bDescriptorType     = USB_INTERFACE_ASSOCIATION_DESCRIPTOR,
+    .bFirstInterface     = USB_INTF_CDC_COMM,
+    .bInterfaceCount     = 2,
+    .bFunctionClass      = USB_CDC_COMM_CLASS,
+    .bFunctionSubClass   = USB_CDC_ACM_SUBCLASS,
+    .bFunctionProtocol   = 0,
+    .iFunction           = 0,
+  },
+
+  .interface_comm =
+  {
+    .bLength             = sizeof(usb_interface_descriptor_t),
+    .bDescriptorType     = USB_INTERFACE_DESCRIPTOR,
+    .bInterfaceNumber    = USB_INTF_CDC_COMM,
+    .bAlternateSetting   = 0,
+    .bNumEndpoints       = 1,
+    .bInterfaceClass     = USB_CDC_COMM_CLASS,
+    .bInterfaceSubClass  = USB_CDC_ACM_SUBCLASS,
+    .bInterfaceProtocol  = 0,
+    .iInterface          = 0,
+  },
+
+  .cdc_header =
+  {
+    .bFunctionalLength   = sizeof(usb_cdc_header_functional_descriptor_t),
+    .bDescriptorType     = USB_CS_INTERFACE_DESCRIPTOR,
+    .bDescriptorSubtype  = USB_CDC_HEADER_SUBTYPE,
+    .bcdCDC              = USB_CDC_BCD_VERSION,
+  },
+
+  .cdc_acm =
+  {
+    .bFunctionalLength   = sizeof(usb_cdc_abstract_control_managment_descriptor_t),
+    .bDescriptorType     = USB_CS_INTERFACE_DESCRIPTOR,
+    .bDescriptorSubtype  = USB_CDC_ACM_SUBTYPE,
+    .bmCapabilities      = USB_CDC_ACM_SUPPORT_LINE_REQUESTS | USB_CDC_ACM_SUPPORT_SENDBREAK_REQUESTS,
+  },
+
+  .cdc_call_mgmt =
+  {
+    .bFunctionalLength   = sizeof(usb_cdc_call_managment_functional_descriptor_t),
+    .bDescriptorType     = USB_CS_INTERFACE_DESCRIPTOR,
+    .bDescriptorSubtype  = USB_CDC_CALL_MGMT_SUBTYPE,
+    .bmCapabilities      = USB_CDC_CALL_MGMT_OVER_DCI,
+    .bDataInterface      = USB_INTF_CDC_DATA,
+  },
+
+  .cdc_union =
+  {
+    .bFunctionalLength   = sizeof(usb_cdc_union_functional_descriptor_t),
+    .bDescriptorType     = USB_CS_INTERFACE_DESCRIPTOR,
+    .bDescriptorSubtype  = USB_CDC_UNION_SUBTYPE,
+    .bMasterInterface    = USB_INTF_CDC_COMM,
+    .bSlaveInterface0    = USB_INTF_CDC_DATA,
+  },
+
+  .ep_comm =
+  {
+    .bLength             = sizeof(usb_endpoint_descriptor_t),
+    .bDescriptorType     = USB_ENDPOINT_DESCRIPTOR,
+    .bEndpointAddress    = USB_IN_ENDPOINT | USB_CDC_EP_COMM,
+    .bmAttributes        = USB_INTERRUPT_ENDPOINT,
+    .wMaxPacketSize      = 64,
+    .bInterval           = 1,
+  },
+
+  .interface_data =
+  {
+    .bLength             = sizeof(usb_interface_descriptor_t),
+    .bDescriptorType     = USB_INTERFACE_DESCRIPTOR,
+    .bInterfaceNumber    = USB_INTF_CDC_DATA,
+    .bAlternateSetting   = 0,
+    .bNumEndpoints       = 2,
+    .bInterfaceClass     = USB_CDC_DATA_CLASS,
+    .bInterfaceSubClass  = USB_CDC_NO_SUBCLASS,
+    .bInterfaceProtocol  = 0,
+    .iInterface          = 0,
+  },
+
+  .ep_in =
+  {
+    .bLength             = sizeof(usb_endpoint_descriptor_t),
+    .bDescriptorType     = USB_ENDPOINT_DESCRIPTOR,
+    .bEndpointAddress    = USB_IN_ENDPOINT | USB_CDC_EP_SEND,
+    .bmAttributes        = USB_BULK_ENDPOINT,
+    .wMaxPacketSize      = 64,
+    .bInterval           = 0,
+  },
+
+  .ep_out =
+  {
+    .bLength             = sizeof(usb_endpoint_descriptor_t),
+    .bDescriptorType     = USB_ENDPOINT_DESCRIPTOR,
+    .bEndpointAddress    = USB_OUT_ENDPOINT | USB_CDC_EP_RECV,
+    .bmAttributes        = USB_BULK_ENDPOINT,
+    .wMaxPacketSize      = 64,
+    .bInterval           = 0,
+  },
+};
+
+const alignas(4) usb_string_descriptor_zero_t usb_string_descriptor_zero =
+{
+  .bLength         = sizeof(usb_string_descriptor_zero_t),
+  .bDescriptorType = USB_STRING_DESCRIPTOR,
+  .wLANGID         = USB_LANGID_ENGLISH,
+};
+
+const char *usb_strings[] =
+{
+  [USB_STR_MANUFACTURER]  = "Alex Taradov",
+  [USB_STR_PRODUCT]       = "Virtual COM-Port",
+  [USB_STR_SERIAL_NUMBER] = usb_serial_number,
+};
+
+const usb_class_handler_t usb_class_handlers[1] =
+{
+  usb_cdc_handle_request,
+};
+
+char usb_serial_number[16];
